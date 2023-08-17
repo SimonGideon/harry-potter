ccaf9be77e41cd9dd0dd524bf6b95398f7b2e91a
SimonGideon <simongideon918@gmail.com>
2023-08-17 19:11:35 +0300

    serch-bar styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={
+          inter.className + " bg-gradient-to-r from-rich-black to-cadet-gray"
+        }
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
--- app/search.jsx
+++ app/search.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-const SearchBar = ({ onSearch}) => {
-    const [searchQuery, setSearchQuery] = React.useState("");
-    const handleSearch = (e) => {
-        setSearchQuery(e.target.value);
-        onSearch(e.target.value);
-    };
-    return (
-        <input
-        type="text"
-        placeholder="Search characters..."
-        value={searchQuery}
-        onChange={handleSearch}
-        />
-    );
+
+const SearchBar = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = React.useState("");
+  
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+    onSearch(e.target.value);
+  };
+  
+  return (
+    <input
+      type="text"
+      placeholder="Search characters..."
+      value={searchQuery}
+      onChange={handleSearch}
+      className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:border-blue-500"
+    />
+  );
 };
 
 export default SearchBar;
