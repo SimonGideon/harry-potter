37271163b68c238f4b1cf8c0d6c5986f4ab116ff
SimonGideon <simongideon918@gmail.com>
2023-08-17 13:58:24 +0300

    Fetch-Display all characters

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,39 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCharacters } from "./redux/characters/characterSlice";
+const Home = () => {
+  const characters = useSelector((state) => state.characters.data);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const error = useSelector((state) => state.characters.error);
 
-export default function Home() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCharacters());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
   return (
     <div>
-      <h1>Home</h1>
+      <h1>All Characters</h1>
+      {characters.map((character) => (
+        <div
+          key={character.id}
+          onClick={() => handleCharacterClick(character.id)}
+        >
+          <h3>{character.name}</h3>
+          <h5>{character.dateOfBirth}</h5>
+          <img src={character.image} alt="image" />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
+
+export default Home;
