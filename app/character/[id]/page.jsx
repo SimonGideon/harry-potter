376847cc1566ed396e29621e35cb6b047bc18c47
SimonGideon <simongideon918@gmail.com>
2023-08-18 00:31:01 +0300

    Fix: Classes for Desktop

--- app/character/[id]/page.jsx
+++ app/character/[id]/page.jsx
@@ -31,47 +31,45 @@ const CharacterDetail = ({ params: { id } }) => {
 
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="col-span-1">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="bio-data">
-              <p className="text-xl font-semibold mb-4">{character.name}</p>
-              <p>Date of Birth: {character.dateOfBirth}</p>
-              <p>Gender: {character.gender}</p>
-              <p>Actor: {character.actor}</p>
-              <p>Status: {character.alive ? "Alive" : "Dead"}</p>
-            </div>
-            <div className="w-1/1">
-              <img
-                src={character.image}
-                alt={character.name}
-                className="mx-auto w-full h-auto rounded-lg"
-              />
-            </div>
+      <div className="grid grid-cols-1 md:grid-cols-1 gap-6 ">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="bio-data">
+            <p className="text-xl font-semibold mb-4">{character.name}</p>
+            <p>Born: {character.dateOfBirth}</p>
+            <p>Gender: {character.gender}</p>
+            <p>Actor: {character.actor}</p>
+            <p>Status: {character.alive ? "Alive" : "Dead"}</p>
           </div>
-          <p className="mt-6">
-            In the world of magic, {character.name} stands out as a{" "}
-            {character.gender === "male" ? "bold" : "courageous"} individual.
-            Born on {character.dateOfBirth}, {character.name} ventured into the
-            realm of wizards and witches with an unwavering spirit.
-          </p>
-
-          <p className="mt-4">
-            Raised in the renowned {character.house} house, {character.name}{" "}
-            learned the secrets of spells and potions, forging lifelong
-            friendships with fellow students. {character.name}'s{" "}
-            {character.gender === "male" ? "bold" : "daring"} actions in the
-            face of adversity earned a place in wizarding history.
-          </p>
-
-          <p className="mt-4">
-            With {character.eyeColour} eyes that reflect {character.hairColour}{" "}
-            hair, {character.name} possesses a wand crafted from{" "}
-            {character.wand.wood}, known for its {character.wand.core} core and
-            a length of {character.wand.length}. This magical tool aids in
-            manifesting spells beyond imagination.
-          </p>
+          <div className="profile-image">
+            <img
+              src={character.image}
+              alt={character.name}
+              className="mx-auto w-1/2 h-auto rounded-lg"
+            />
+          </div>
         </div>
+        <p className="mt-6">
+          In the world of magic, {character.name} stands out as a{" "}
+          {character.gender === "male" ? "bold" : "courageous"} individual. Born
+          on {character.dateOfBirth}, {character.name} ventured into the realm
+          of wizards and witches with an unwavering spirit.
+        </p>
+
+        <p className="mt-4">
+          Raised in the renowned {character.house} house, {character.name}{" "}
+          learned the secrets of spells and potions, forging lifelong
+          friendships with fellow students. {character.name}'s{" "}
+          {character.gender === "male" ? "bold" : "daring"} actions in the face
+          of adversity earned a place in wizarding history.
+        </p>
+
+        <p className="mt-4">
+          With {character.eyeColour} eyes that reflect {character.hairColour}{" "}
+          hair, {character.name} possesses a wand crafted from{" "}
+          {character.wand.wood}, known for its {character.wand.core} core and a
+          length of {character.wand.length}. This magical tool aids in
+          manifesting spells beyond imagination.
+        </p>
       </div>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -35,57 +35,60 @@ const Home = () => {
     }
   };
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="p-10 md:p-12">
       <SearchBar onSearch={handleSearch} />
       <h1 className="text-3xl font-bold mt-4 md:mt-8">All Characters</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 mt-4 md:mt-8">
-        {isLoading ? (
-          <div className="text-center">Loading...</div>
-        ) : error ? (
-          <div className="text-center text-red-500">Error: {error}</div>
-        ) : filteredCharacters.length === 0 ? (
-          <p className="text-center">No characters found.</p>
-        ) : (
-          filteredCharacters.map((character) => (
-            <Link href={`/character/${character.id}`} key={character.id}>
-              <div className="card hover:shadow-md cursor-pointer">
-                <div className="relative">
-                  <div className="flex justify-center items-end h-50">
-                    <img
-                      className="rounded-2xl w-full object-contain"
-                      src={character.image}
-                      alt={character.alternate_names[0]}
-                    />
-                  </div>
-                  <div className="absolute bottom-0">
-                    <span className={myFont.className}>
-                      <h1 className="name absolute text-yellow-400 text-4xl  left-1/2 transform -translate-x-1/2 bottom-5 bottom-2 text-center">
-                        {character.name}
-                      </h1>
-                    </span>
-                    <Image
-                      className="rounded-2xl"
-                      src="/2.png"
-                      width={500}
-                      height={500}
-                      alt="Picture of the author"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <h3 className="text-lg font-semibold mt-2">
-                    Actor: {character.actor}
-                  </h3>
-                  <h5 className="text-gray-600">
-                    D.O.B {character.dateOfBirth}
-                  </h5>
-                </div>
+         {filteredCharacters.map((character) => (
+        <Link href={`/character/${character.id}`} key={character.id}>
+          <div className="card hover:shadow-md cursor-pointer">
+            <div className="relative">
+              <div className="flex justify-center items-end h-50">
+                <img
+                  className="rounded-2xl w-full object-contain"
+                  src={character.image}
+                  alt={character.alternate_names[0]}
+                />
               </div>
-            </Link>
-          ))
-        )}
+              <div className="absolute bottom-0">
+                <span className={myFont.className}>
+                  <h1 className="name absolute text-yellow-400 text-4xl  left-1/2 transform -translate-x-1/2 bottom-5 bottom-2 text-center">
+                    {character.name}
+                  </h1>
+                </span>
+                <Image
+                  className="rounded-2xl"
+                  src="/2.png"
+                  width={500}
+                  height={500}
+                  alt="Picture of the author"
+                />
+              </div>
+            </div>
+
+            <div>
+              <h3 className="text-lg font-semibold mt-2">
+                Actor: {character.actor}
+              </h3>
+              <h5 className="text-gray-600">
+                D.O.B{" "}
+                {character.dateOfBirth
+                  ? character.dateOfBirth
+                  : character.yearOfBirth}
+              </h5>
+            </div>
+          </div>
+        </Link>
+        ))}
       </div>
     </div>
   );
