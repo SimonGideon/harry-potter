2a0a3c03bddec1a688caf4d80c92a1241aac2bcf
SimonGideon <simongideon918@gmail.com>
2023-08-17 14:16:28 +0300

    fetch individual data

--- app/character/[id]/page.jsx
+++ app/character/[id]/page.jsx
@@ -1,9 +1,34 @@
-const characterDetails = () => {
-    return ( 
-        <div>
-            <h1>Character Details</h1>
-        </div>
-     );
-}
- 
-export default characterDetails;
+"use client";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCharacterById } from "../../redux/characters/characterSlice";
+
+const characterDetail = ({ params: { id } }) => {
+  const dispatch = useDispatch();
+  const { selectedCharacter, isLoading, error } = useSelector(
+    (state) => state.characters
+  );
+
+  useEffect(() => {
+    dispatch(fetchCharacterById(id));
+  }, [dispatch, id]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  const character = selectedCharacter[0];
+
+  return (
+    <div>
+      <h1>Character {id}</h1>
+      <p>Name: {character?.name}</p>
+      <p>Species: {character?.species}</p>
+      <p>Gender: {character?.gender}</p>
+    </div>
+  );
+};
+export default characterDetail;
